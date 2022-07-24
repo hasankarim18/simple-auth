15eb25cba2e7a40e042095adaf2fe8705979eb76
hasankarim18 <66988931+hasankarim18@users.noreply.github.com>
2022-07-24 10:15:21 +0600

    UseEffect start

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
@@ -9,6 +9,7 @@ function App() {
 
   const loginHandler = (email, password) => {
     ///
+    localStorage.setItem('isLoggedIn', '1')
     //
     setIsLoggedIn(true)
   }
@@ -17,6 +18,13 @@ function App() {
     setIsLoggedIn(false)
   }
 
+  const sotredUserLoggedInInformation = localStorage.getItem('isLoggedIn')
+
+  useEffect(() => {
+    if (sotredUserLoggedInInformation === '1') {
+      setIsLoggedIn(true)
+    }
+  }, [])
 
   return (
     <React.Fragment>
