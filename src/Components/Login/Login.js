9108d2051661c99836327438dd7c14170d806355
hasankarim18 <66988931+hasankarim18@users.noreply.github.com>
2022-07-24 12:24:02 +0600

    form wont valid useEffect only run once and dont have dependencies

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -12,23 +12,18 @@ const Login = (props) => {
     const [formIsValid, setFormIsValid] = useState(false);
 
 
-
+    useEffect(() => {
+        setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6)
+    }, [])
 
 
     const emailChangeHandler = (event) => {
         setEnteredEmail(event.target.value);
-
-        setFormIsValid(
-            event.target.value.includes('@') && enteredPassword.trim().length > 6
-        );
     };
 
     const passwordChangeHandler = (event) => {
         setEnteredPassword(event.target.value);
 
-        setFormIsValid(
-            event.target.value.trim().length > 6 && enteredEmail.includes('@')
-        );
     };
 
     const validateEmailHandler = () => {
